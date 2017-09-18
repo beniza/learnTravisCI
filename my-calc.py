01ca5be9761499e4ce315d0e597632eecf661086
benVar <beniza@users.noreply.github.com>
2017-09-18 15:30:42 +0530

    Format for lynt

--- my-calc.py
+++ my-calc.py
@@ -5,24 +5,24 @@ a = 10
 b = 2
 
 def addition(x, y):
-	return a + b
+    return a + b
 
-def sum(a,b):
-	return (a + b)
+def sum(a, b):
+    return a + b
 
 def division(x, y):
-	if(y!=0):
-		return(x/y)
-	else:
-		return("Division by Zero not defined")
+    if y != 0:
+        return x / y
+    else:
+        return "Division by Zero not defined"
 
-def mul(a,b):
-    return(a*b)
+def mul(a, b):
+    return a * b 
 
 def subtraction(a, b):
-	return(a-b)
+    return a - b
 
-def mod(a,b):
-	return (a%b)
+def mod(a, b):
+    return a % b
 
 print "Calculator App"
